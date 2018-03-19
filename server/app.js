c408e91f5655ca24467142a463601655cb5eed56
Chas Parr <cxp2265@rit.edu>
2018-03-19 11:31:59 -0400

    bugfixes

--- server/app.js
+++ server/app.js
@@ -12,20 +12,20 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-    if (err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
 const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`$__dirname}/../hosted/img/favicon.png`));
+app.use(favicon('$__dirname}/../hosted/img/favicon.png'));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -34,8 +34,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-    if (err) {
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
-})
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
